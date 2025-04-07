70e720c2fc3a5b42263828741c1f6496fedf124a
Shashika Jayasundara <shashikab@wso2.com>
2025-04-07 20:31:48 +0530

    c

--- apictl/deploy-api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Step 1: Log in to APIM
-apictl login dev --insecure -u admin -p admin
-
-# Step 2: Import (or update) the API project
-./apictl import api --file ./SampleAPI --environment dev -k --rotate-revision
